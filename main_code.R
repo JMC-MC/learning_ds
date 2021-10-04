d9b27f99cdb2bde1093ba3befa72ea5e5116f3ec
JMC-MC <joecurrin@Joes-MacBook-Pro.local>
2021-10-04 15:15:07 +1100

    Tidy up code

--- main_code.R
+++ main_code.R
@@ -1,3 +1,9 @@
+##################################################
+
+# Main code for Movielens Project EDX 
+
+##################################################
+
 # Load libraries
 library(tidyverse)
 library(caret)
@@ -6,21 +12,16 @@ library(stringr)
 library(lubridate)
 library(ggridges)
 
-# Load edx data frame
+# Load edx data frames
 load("movielens_edx.Rda")
+load("movielens_validation.Rda")
 
-# Quiz
+##################################################
 
-q2_1 <- nrow(edx[edx$rating == 0])
-q2_2 <- nrow(edx[edx$rating == 3])
-q3 <- length(unique(edx$movieId))
-q4 <- length(unique(edx$userId))
-q5 <- edx %>% filter(str_detect(genres,"Romance"))
-q6 <- edx %>% group_by(rating) %>% summarise(no = n()) %>% arrange(by= desc(no))
-q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= desc(no_ratings))
-####################################
+# Data cleaning
 
-# Data Cleaning 
+##################################################
+
   #Completeness Check
     #Check for NAs and blanks
       na <- edx[rowSums(is.na(edx)) > 0,] # none
@@ -53,28 +54,37 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
   # Correct WotW and extract year
   edx_y <- WotW_corr(edx)
   edx_y <- ext_year(edx_y)
+  
+    # Correction process for validation
+      validation <- WotW_corr(validation)
+      validation <- ext_year(validation)
 
-# Date Exploration
+##################################################
+      
+# Exploring data to determine relationships
+      
+##################################################
   # Ratings
-  rtngs <- edx_y %>% group_by(rating) %>%
-    ggplot(aes(rating)) + geom_bar()
-  rtngs
+    rtngs <- edx_y %>% group_by(rating) %>%
+      ggplot(aes(rating)) + geom_bar()
+    rtngs
+  
   # Reviews per user 
-  usr_rev_plot <- edx_y %>% group_by(userId) %>% summarise(no_reviews = n()) %>% 
-    ggplot(aes(no_reviews)) + geom_histogram(bin=2) + xlim(0,1000) 
-  usr_rev_plot
-  usr_rev_sum <- edx_y %>% group_by(userId) %>% summarise(no_reviews = n()) %>%
-    select(no_reviews) %>% summary()
-  usr_rev_sum
+    usr_rev_plot <- edx_y %>% group_by(userId) %>% summarise(no_reviews = n()) %>% 
+      ggplot(aes(no_reviews)) + geom_histogram(bin=2) + xlim(0,1000) 
+    usr_rev_plot
+    usr_rev_sum <- edx_y %>% group_by(userId) %>% summarise(no_reviews = n()) %>%
+      select(no_reviews) %>% summary()
+    usr_rev_sum
   
   # Reviews per movie
   
-  mv_rev_plot <- edx_y %>% group_by(movieId) %>% summarise(no_reviews = n()) %>% 
-    ggplot(aes(no_reviews)) + geom_histogram(bin=2) + xlim(0,1000) 
-  mv_rev_plot
-  mv_rev_sum <- edx_y %>% group_by(movieId) %>% summarise(no_reviews = n()) %>%
-    select(no_reviews) %>% summary()
-  usr_rev_sum
+    mv_rev_plot <- edx_y %>% group_by(movieId) %>% summarise(no_reviews = n()) %>% 
+      ggplot(aes(no_reviews)) + geom_histogram(bin=2) + xlim(0,1000) 
+    mv_rev_plot
+    mv_rev_sum <- edx_y %>% group_by(movieId) %>% summarise(no_reviews = n()) %>%
+      select(no_reviews) %>% summary()
+    usr_rev_sum
   
   # Relationship rating - time of day
   
@@ -88,10 +98,10 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
     genres_rts <-  edx_y %>% group_by(genres) %>% 
       summarise(avg_rating=mean(rating), no_reviews = n(), no_movies = length(unique(movieId)))
     # lowest avg rating by genre
-    genres_rts %>% arrange(by=avg_rating) %>% slice(1:10)
+      genres_rts %>% arrange(by=avg_rating) %>% slice(1:10)
 
     # highest avg rating by genre
-    genres_rts %>% arrange(by=desc(avg_rating)) %>% slice(1:10)
+      genres_rts %>% arrange(by=desc(avg_rating)) %>% slice(1:10)
   
   # Relationship rating - year
     yr_rts <-  edx_y %>% group_by(year) %>% 
@@ -104,17 +114,22 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
        ggplot(aes(ysr, avg_rating)) + geom_point(alpha = 0.5,)
      
      # Can the function be modeled by knn?
-     fit_ysr_knn <- train_set %>% mutate(ysr = year(as_datetime(timestamp))-year) %>% 
+      fit_ysr_knn <- train_set %>% mutate(ysr = year(as_datetime(timestamp))-year) %>% 
        train(rating ~ ysr, data = ., method="knn")
-     ysr <- seq(1,80,1)
-     prediction <- predict(fit_ysr_knn,ysr)
+      ysr <- seq(1,80,1)
+      prediction <- predict(fit_ysr_knn,ysr)
      
-     as_data_frame(prediction)%>% mutate(ysr=ysr) %>% 
+      as_data_frame(prediction)%>% mutate(ysr=ysr) %>% 
        ggplot(aes(ysr,prediction)) + geom_line()
      
-# Developing a model
+##################################################
+      
+  # Training the model
+      
+##################################################
   
   # Divide training and testing
+     
      test_index <- createDataPartition(y = edx_y$rating, times = 1, p = 0.2, 
                                        list = FALSE)
      train_set <- edx_y[-test_index,]
@@ -124,45 +139,36 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
        semi_join(train_set, by = "movieId") %>%
        semi_join(train_set, by = "userId")
      
-     # Trying loess
+  # Using loess to smooth YSR data and predict a rating for each value of YSR. 
      
      trial_index <- createDataPartition(y = train_set$rating, times = 1, p = 0.10, 
                                         list = FALSE)
      trial_set <- train_set[trial_index,]
-     nrow(trial_set)
-     # Timing computation time
-     # Start the clock!
-     ptm <- proc.time()
+
      span <- 0.35
      fit_1 <- trial_set %>% 
        mutate(ysr = year(as_datetime(timestamp))-year) %>% 
        loess(rating ~ ysr, degree=1, span = span, data=.)
-     
-     # Stop the clock
-     time <- proc.time() - ptm
-     time[3]
-     
+
      ysr_range <- seq(-2,93,1)
      
      ysr_bias <- train_set %>% 
        mutate(ysr = year(as_datetime(timestamp))-year, ysr_p = predict(fit_1,ysr)) %>%
        group_by(ysr) %>% summarise(ysr_b = mean(ysr_p))
      
-     ysr_bias
-     
-     # Fill in the N/As
+     # Fill in the N/As with closest prediction
      
      ysr_bias$ysr_b[1] = ysr_bias$ysr_b[2]
      
      ysr_bias$ysr_b[96] = ysr_bias$ysr_b[95]
      
-  # Average rating
-    avg_rating <- mean(train_set$rating)
-    l=5
+  # Set Lamda based on optimization see end of file for code sample
+    
+     l=5
     
   # Modeling movie bias with regularization
     
-    movie_bias <- train_set %>%
+    movie_bias <- edx_y %>%
       mutate(ysr = year(as_datetime(timestamp))-year) %>%
       left_join(ysr_bias,by = "ysr") %>%
       group_by(movieId) %>%
@@ -170,73 +176,28 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
     
   # Modeling user bias with regularization
     
-    user_bias <- train_set %>%
+    user_bias <- edx_y %>%
       mutate(ysr = year(as_datetime(timestamp))-year) %>%
       left_join(ysr_bias,by = "ysr") %>%
       left_join(movie_bias, by="movieId") %>%
       group_by(userId) %>%
       summarize(usr_b = sum(rating - mv_b - ysr_b)/(n()+l))
   
-  # Modeling genre bias
-    genre_bias <- train_set %>% 
+  # Modeling genre bias with regularization
+    genre_bias <- edx_y %>% 
       mutate(ysr = year(as_datetime(timestamp))-year) %>%
       left_join(ysr_bias,by = "ysr") %>%
       left_join(user_bias, by="userId") %>%
       left_join(movie_bias, by="movieId") %>%
       group_by(genres) %>% summarize(gnr_b = sum(rating - usr_b - mv_b - ysr_b)/(n()+l))
+
+##################################################
     
-
-  
-
-    # # Optimize lamda for regularization
-    # lambdas <- seq(0, 20, 0.25)
-    # 
-    # rmses <- sapply(lambdas, function(l){
-    #   
-    #   # Modeling movie bias with regularization
-    #   
-    #   movie_bias <- train_set %>%
-    #     mutate(ysr = year(as_datetime(timestamp))-year) %>%
-    #     left_join(ysr_bias,by = "ysr") %>%
-    #     group_by(movieId) %>%
-    #     summarize(mv_b = sum(rating - ysr_b)/(n()+l))
-    #   
-    #   # Modeling user bias with regularization
-    #   
-    #   user_bias <- train_set %>%
-    #     mutate(ysr = year(as_datetime(timestamp))-year) %>%
-    #     left_join(ysr_bias,by = "ysr") %>%
-    #     left_join(movie_bias, by="movieId") %>%
-    #     group_by(userId) %>%
-    #     summarize(usr_b = sum(rating - mv_b - ysr_b)/(n()+l))
-    #   
-    #   # Modeling genre bias
-    #   genre_bias <- train_set %>% 
-    #     mutate(ysr = year(as_datetime(timestamp))-year) %>%
-    #     left_join(ysr_bias,by = "ysr") %>%
-    #     left_join(user_bias, by="userId") %>%
-    #     left_join(movie_bias, by="movieId") %>%
-    #     group_by(genres) %>%
-    #     summarize(gnr_b = sum(rating - usr_b - mv_b - ysr_b)/(n()+l))
-    # 
-    # 
-    #   y_hat <-  test_set %>% 
-    #     left_join(user_bias, by="userId") %>%
-    #     left_join(movie_bias, by="movieId") %>%
-    #     left_join(genre_bias, by="genres") %>% 
-    #     mutate(ysr = year(as_datetime(timestamp))-year) %>%
-    #     left_join(ysr_bias, by="ysr") %>%
-    #     mutate(p_rating = ysr_b + usr_b + mv_b + gnr_b)
-    # 
-    #   return(RMSE(test_set$rating, y_hat$p_rating))
-    # })
-    # 
-    # qplot(lambdas, rmses)
-
-# Making final predictions
-  
+# Making predictions
     
-    y_hat <-  test_set %>% 
+##################################################
+    
+    y_hat <-  validation %>% 
       left_join(user_bias, by="userId") %>%
       left_join(movie_bias, by="movieId") %>%
       left_join(genre_bias, by="genres") %>% 
@@ -244,32 +205,105 @@ q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= d
       left_join(ysr_bias, by="ysr") %>%
       mutate(p_rating = ysr_b + usr_b + mv_b + gnr_b)
 
-    summary(y_hat)
-
     # Make sure predictions are within rating range
+    
+    y_hat %>% select(p_rating) %>% summary 
     
     y_hat$p_rating[y_hat$p_rating<0] = 0
     
     y_hat$p_rating[y_hat$p_rating>5] = 5
 
-    y_hat %>% select(p_rating) %>% summary 
- 
-  # Inspect errors
+##################################################
     
-    error_hist <- y_hat %>% mutate(error = rating - p_rating) %>% ggplot(aes(error)) + geom_histogram()
-    error_hist
-    y_hat %>% mutate(error = abs(rating - p_rating)) %>% group_by(title, movieId) %>% 
-      summarise(avg_error= mean(error),no_reviews = n(), mv_b = first(mv_b)) %>%
-       arrange(desc(avg_error)) %>% top_n(10)
-
-  train_set %>% filter(userId==4043) %>% ggplot(aes(rating)) + geom_histogram()
-# Calculate RMSE
+# Measuring accuracy 
+    
+##################################################
   
   RMSE <- function(ratings, p_hat){
     sqrt(mean((ratings - p_hat)^2))
   }
   
-  RMSE(test_set$rating, y_hat$p_rating)
+  RMSE(validation$rating, y_hat$p_rating)
 
- 
+##################################################
   
+  # Code used during development of model
+  
+##################################################
+  
+  # Optimize lamda for regularization
+  lambdas <- seq(0, 20, 0.25)
+
+  rmses <- sapply(lambdas, function(l){
+
+    # Modeling movie bias with regularization
+
+    movie_bias <- train_set %>%
+      mutate(ysr = year(as_datetime(timestamp))-year) %>%
+      left_join(ysr_bias,by = "ysr") %>%
+      group_by(movieId) %>%
+      summarize(mv_b = sum(rating - ysr_b)/(n()+l))
+
+    # Modeling user bias with regularization
+
+    user_bias <- train_set %>%
+      mutate(ysr = year(as_datetime(timestamp))-year) %>%
+      left_join(ysr_bias,by = "ysr") %>%
+      left_join(movie_bias, by="movieId") %>%
+      group_by(userId) %>%
+      summarize(usr_b = sum(rating - mv_b - ysr_b)/(n()+l))
+
+    # Modeling genre bias
+    genre_bias <- train_set %>%
+      mutate(ysr = year(as_datetime(timestamp))-year) %>%
+      left_join(ysr_bias,by = "ysr") %>%
+      left_join(user_bias, by="userId") %>%
+      left_join(movie_bias, by="movieId") %>%
+      group_by(genres) %>%
+      summarize(gnr_b = sum(rating - usr_b - mv_b - ysr_b)/(n()+l))
+
+
+    y_hat <-  test_set %>%
+      left_join(user_bias, by="userId") %>%
+      left_join(movie_bias, by="movieId") %>%
+      left_join(genre_bias, by="genres") %>%
+      mutate(ysr = year(as_datetime(timestamp))-year) %>%
+      left_join(ysr_bias, by="ysr") %>%
+      mutate(p_rating = ysr_b + usr_b + mv_b + gnr_b)
+
+    return(RMSE(test_set$rating, y_hat$p_rating))
+  })
+
+  qplot(lambdas, rmses)
+  
+  # Inspect errors
+  
+  error_hist <- y_hat %>% mutate(error = rating - p_rating) %>% ggplot(aes(error)) + geom_histogram()
+  error_hist
+  y_hat %>% mutate(error = abs(rating - p_rating)) %>% group_by(title, movieId) %>% 
+    summarise(avg_error= mean(error),no_reviews = n(), mv_b = first(mv_b)) %>%
+    arrange(desc(avg_error)) %>% top_n(10)
+  
+  train_set %>% filter(userId==4043) %>% ggplot(aes(rating)) + geom_histogram()
+  
+  # Timing smoothing function to determine an appropirate sample size and span
+  # Start the clock!
+  ptm <- proc.time()
+  span <- 0.35
+  fit_1 <- trial_set %>% 
+    mutate(ysr = year(as_datetime(timestamp))-year) %>% 
+    loess(rating ~ ysr, degree=1, span = span, data=.)
+  
+  # Stop the clock
+  time <- proc.time() - ptm
+  time[3]
+  
+  # Quiz Answers
+  
+  q2_1 <- nrow(edx[edx$rating == 0])
+  q2_2 <- nrow(edx[edx$rating == 3])
+  q3 <- length(unique(edx$movieId))
+  q4 <- length(unique(edx$userId))
+  q5 <- edx %>% filter(str_detect(genres,"Romance"))
+  q6 <- edx %>% group_by(rating) %>% summarise(no = n()) %>% arrange(by= desc(no))
+  q7 <- edx %>% group_by(rating) %>% summarise(no_ratings = n()) %>% arrange(by= desc(no_ratings))
